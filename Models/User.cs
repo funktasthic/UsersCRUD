5af730dc8c63c511e14611110011be96cdf437a1
funktasthic <ignacio.avendano@alumnos.ucn.cl>
2025-09-23 22:21:43 -0300

    feat: add user model

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+namespace UserManagementApi.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; } = null!;
+        [Required, MaxLength(15)]
+        public string Name { get; set; } = null!;
+        [Required, MaxLength(100)]
+        public string LastName { get; set; } = null!;
+        [Required, MaxLength(100), EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required, MaxLength(30)]
+        public string PasswordHash { get; set; } = null!;
+        public bool IsActive { get; set; } = true;
+    }
+}
